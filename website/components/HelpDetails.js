acd30b3dc6974f114311f1676be8107e0a174089
Richard Zhang <richardzcode@gmail.com>
2018-02-27 10:39:44 -0800

    help page improvement

--- website/components/HelpDetails.js
+++ website/components/HelpDetails.js
@@ -1,16 +1,18 @@
 const React = require('react');
-const { H3, Div, P, Row, Col } = require('fluid-react');
+const { H3, Div, Row, Col } = require('fluid-react');
 
+const MarkdownBlock = require('./MarkdownBlock.js');
 const help = require('./help.json');
 
 const HelpDetails = (props) => {
-  const { theme } = props.site;
+  const { site, lang } = props;
+  const { theme } = site;
   const sectionComps = help.map((help_item, idx) => {
     return (
       <Col col={4} key={idx}>
         <Div style={theme.section}>
           <H3 style={theme.helpSectionTitle}>{help_item.title}</H3>
-          <P style={theme.p} dangerouslySetInnerHTML={{__html: help_item.content}} />
+          <MarkdownBlock site={site} lang={lang}>{help_item.content}</MarkdownBlock>
         </Div>
       </Col>
     )
